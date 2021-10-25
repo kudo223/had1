a0897a80d70facbf04ec6da911b61cd50ec54292
Dominik Kunc <90252867+kudo223@users.noreply.github.com>
2021-10-25 22:18:09 +0200

    Update Form1.cs

--- had1/Form1.cs
+++ had1/Form1.cs
@@ -13,22 +13,26 @@ namespace had1
     public partial class Form1 : Form
     {
         PictureBox had1;
-        PictureBox had2;
         List<PictureBox> ocasHada1;
-        List<PictureBox> ocasHada2;
         double smer1;
-        double smer2;
         double rychlost1;
+
+        PictureBox had2;
+        List<PictureBox> ocasHada2;
         double rychlost2;
+        double smer2;
+
+        Timer timer;
+
         public Form1()
         {
             InitializeComponent();
             this.KeyPreview = true;
             this.KeyDown += Form1_KeyDown;
+            this.KeyDown += Form2_KeyDown;
             VytvorHady();
-            Timer timer;
             timer = new Timer();
-            timer.Interval = 500;
+            timer.Interval = 50;
             timer.Tick += Timer_Tick;
             timer.Start();
         }
@@ -43,13 +47,13 @@ namespace had1
                 smer1 -= 0.1;
             }
         }
-        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        private void Form2_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.W)
+            if (e.KeyCode == Keys.Left)
             {
                 smer1 += 0.1;
             }
-            else if (e.KeyCode == Keys.S)
+            else if (e.KeyCode == Keys.Right)
             {
                 smer1 -= 0.1;
             }
@@ -59,40 +63,43 @@ namespace had1
         private void Timer_Tick(object sender, EventArgs e)
         {
             ocasHada1.Add(had1);
-            ocasHada2.Add(had2);
-            smer1 = 0;
-            smer2 = 0;
-            rychlost1 = 2;
-            rychlost2 = 2;
             had1 = new PictureBox();
-            had2 = new PictureBox();
             had1.Size = new Size(10, 10);
-            had2.Size = new Size(10, 10);
             var zmenaX = rychlost1 * Math.Cos(smer1);
-            var zmenaX2 = rychlost2 * Math.Cos(smer2);
             var zmenaY = rychlost1 * -Math.Sin(smer1);
-            var zmenaY2 = rychlost2 * -Math.Sin(smer2);
-            had1.Location = new Point(ocasHada1.Last().Location.X + Convert.ToInt32(zmenaX), ocasHada1.Last().Location.Y + Convert.ToInt32(zmenaY));
-            had2.Location = new Point(ocasHada2.Last().Location.X + Convert.ToInt32(zmenaX2), ocasHada2.Last().Location.Y + Convert.ToInt32(zmenaY2));
+            had1.Location = new Point(ocasHada1.Last().Location.X + (int)zmenaX, ocasHada1.Last().Location.Y + (int)zmenaY);
             had1.BackColor = Color.Blue;
+            Controls.Add(had1);
+
+            ocasHada2.Add(had2);
+            had2 = new PictureBox();
+            had2.Size = new Size(10, 10);
+            var zmenaX2 = rychlost2 * Math.Cos(smer2);
+            var zmenaY2 = rychlost2 * -Math.Sin(smer2);
+            had2.Location = new Point(ocasHada2.Last().Location.X + (int)zmenaX2, ocasHada2.Last().Location.Y + (int)zmenaY2);
             had2.BackColor = Color.Red;
-            this.Controls.Add(had1);
-            this.Controls.Add(had2);
+            Controls.Add(had2);
+
         }
         private void VytvorHady()
         {
             ocasHada1 = new List<PictureBox>();
+            smer1 = 0;
+            rychlost1 = 2;
             had1 = new PictureBox();
             had1.Size = new Size(10, 10);
             had1.Location = new Point(10, 10);
             had1.BackColor = Color.Blue;
-            this.Controls.Add(had1);
+            Controls.Add(had1);
+
             ocasHada2 = new List<PictureBox>();
+            smer2 = 0;
+            rychlost2 = -2;
             had2 = new PictureBox();
             had2.Size = new Size(10, 10);
             had2.Location = new Point(935, 10);
             had2.BackColor = Color.Red;
-            this.Controls.Add(had2);
+            Controls.Add(had2);
         }
     }
 }
