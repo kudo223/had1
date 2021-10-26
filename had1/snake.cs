364744b36aedd3d52f330f2c0ea46e1fb6d0a3e0
Dominik Kunc <Kunc.Dominik@stud.vassboskovice.cz>
2021-10-26 13:43:50 +0200

    trida had

---
+++ had1/snake.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace had1
+{
+    public class Snake
+    {
+        public List<PointF> Tail { get; set; }
+        public PointF Head { get; set; }
+        public double Speed { get; set; }
+        public double Direction { get; set; }
+
+        public Snake()
+        {
+            Head = new PointF();
+            Tail = new List<PointF>();
+        }
+
+        public void Forward()
+        {
+            Tail.Add(Head);
+            var dx = Speed * Math.Cos(Direction);
+            var dy = Speed * -Math.Sin(Speed);
+            Head = new PointF(Convert.ToSingle(Tail[Tail.Count - 1].X + dx), Convert.ToSingle(Tail[Tail.Count - 1].Y + dy));
+        }
+        public void Left()
+        {
+            Direction += 0.3;
+        }
+        public void Right()
+        {
+            Direction -= 0.3;
+        }
+
+        public List<PictureBox> GetPictureBoxes(Color color, int size)
+        {
+            List<PictureBox> pictureBoxes = new List<PictureBox>();
+            foreach(var point in Tail)
+            {
+                pictureBoxes.Add(new PictureBox() { Location = new Point(), BackColor = color });
+            }
+            return pictureBoxes;
+        }
+    }
+}
